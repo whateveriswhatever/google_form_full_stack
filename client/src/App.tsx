d77a3fbcb071ff04b4e757cc60a9e68b626fb5fc
whoever-you-wanna-think <banvietthenaocungdc@gmail.com>
2024-01-05 17:09:28 +0700

    Erected the nav-bar1

---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Header from "./components/Header";
+
+let App = () => {
+  return (
+    <div>
+      {/* <h2>Velvet Form</h2> */}
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import NavBar1 from "../sections/HeaderSections/NavBar1";
+
+const Header = () => {
+  return (
+    <div>
+      <NavBar1 />
+    </div>
+  );
+};
+
+export default Header;
